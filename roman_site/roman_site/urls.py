dba4c44f9fb71d5b6d1cadd1286ce43480ef2563
Michael <michaelpapen14@gmail.com>
2026-03-25 21:15:54 +0400

    fix

--- roman_site/roman_site/urls.py
+++ roman_site/roman_site/urls.py
@@ -7,13 +7,4 @@ urlpatterns = [
     path('posts/', include(('posts.urls', 'posts'), namespace='posts')),
     path('', include(('pages.urls', 'pages'), namespace='pages')),
     path('auth/', include('django.contrib.auth.urls')),
-    path(
-        'auth/registration/', 
-        CreateView.as_view(
-            template_name='registration/registration_form.html',
-            form_class=UserCreationForm,
-            success_url=reverse_lazy('pages:homepage'),
-        ),
-        name='registration',
-    ),
 ]
